67277546f5ed2b7ae19370d01f20d1b17e6398f2
Antonio José <bdez_1997@live.com>
2021-10-25 14:44:46 +0200

    Pa seguir en casax2

--- app/src/main/java/com/bermudez/calculadora/SumarActivity.java
+++ app/src/main/java/com/bermudez/calculadora/SumarActivity.java
@@ -3,15 +3,35 @@ package com.bermudez.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SumarActivity extends AppCompatActivity {
-
+    Button btnCalcularSuma;
+    EditText numberSuma,numberSuma2;
+    TextView txtResultadoSuma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sumar);
         setTitle("Sumar");
-
+        btnCalcularSuma=findViewById(R.id.btnCalcularSuma);
+        numberSuma=findViewById(R.id.editTextNumberSumar);
+        numberSuma2=findViewById(R.id.editTextNumberSumar2);
+        txtResultadoSuma=findViewById(R.id.txtResultadoSuma);
+        btnCalcularSuma.setOnClickListener(e->{
+            txtResultadoSuma.setText(""+sumar());
+        });
 
     }
+    public int sumar() {
+        int num1,num2,iSuma;
+
+        num1=Integer.parseInt(numberSuma.getText().toString());
+        num2=Integer.parseInt(numberSuma2.getText().toString());
+        iSuma=num1+num2;
+        return iSuma;
+    }
+
 }
